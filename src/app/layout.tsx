3e3231aa10fc31d7c4ff66920c0f3ccb9575f6c1
Freddy Bremseth <freddy.bremseth@gmail.com>
2026-09-08 20:59:49 +0200

    Load premium gallery design layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import "./mobile-2027.css";
 import "./inland-2027.css";
 import "./inland-town-2027.css";
 import "./assessment-2027.css";
+import "./gallery-2027.css";
 
 const zenecoSans = Inter({
   subsets: ["latin"],
